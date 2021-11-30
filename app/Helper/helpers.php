e44e584d13dd9947b53ca4775102e1529ddb943c
valasys-vpm <87777274+valasys-vpm@users.noreply.github.com>
2021-11-30 20:53:43 +0530

    added time tracker model

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -2,6 +2,7 @@
 
 use App\Repository\Campaign\History\CampaignHistoryRepository;
 use App\Repository\History\HistoryRepository;
+use App\Repository\TimeTracker\TimeTrackerRepository;
 
 if(!function_exists('add_campaign_history')) {
     function add_campaign_history($campaign_id, $parent_campaign_id, $message, $data = array())
@@ -29,5 +30,59 @@ if(!function_exists('add_history')) {
             'data' => $data
         );
         $response = HistoryRepository::store($attributes);
+    }
+}
+
+if(!function_exists('add_time_tracker')) {
+    function add_time_tracker($attributes): array
+    {
+        if(isset($attributes['user_id']) && !empty($attributes['user_id'])) {
+            $data['user_id'] = $attributes['user_id'];
+        }
+
+        if(isset($attributes['time_in']) && !empty($attributes['time_in'])) {
+            $data['time_in'] = $attributes['time_in'];
+        }
+
+        if(isset($attributes['time_out']) && !empty($attributes['time_out'])) {
+            $data['time_out'] = $attributes['time_out'];
+        }
+
+        if(isset($attributes['reason']) && !empty($attributes['reason'])) {
+            $data['reason'] = $attributes['reason'];
+        }
+
+        if(isset($attributes['type']) && !empty($attributes['type'])) {
+            $data['type'] = $attributes['type'];
+        }
+
+        return TimeTrackerRepository::store($data);
+    }
+}
+
+if(!function_exists('update_time_tracker')) {
+    function update_time_tracker($id, $attributes): array
+    {
+        if(isset($attributes['user_id']) && !empty($attributes['user_id'])) {
+            $data['user_id'] = $attributes['user_id'];
+        }
+
+        if(isset($attributes['time_in']) && !empty($attributes['time_in'])) {
+            $data['time_in'] = $attributes['time_in'];
+        }
+
+        if(isset($attributes['time_out']) && !empty($attributes['time_out'])) {
+            $data['time_out'] = $attributes['time_out'];
+        }
+
+        if(isset($attributes['reason']) && !empty($attributes['reason'])) {
+            $data['reason'] = $attributes['reason'];
+        }
+
+        if(isset($attributes['type']) && !empty($attributes['type'])) {
+            $data['type'] = $attributes['type'];
+        }
+
+        return TimeTrackerRepository::update($id, $data);
     }
 }
--- database/migrations/2021_11_30_201637_create_time_trackers_table.php
+++ database/migrations/2021_11_30_201637_create_time_trackers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCampaignHistoriesTable extends Migration
+class CreateTimeTrackersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,22 +13,22 @@ class CreateCampaignHistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('campaign_histories', function (Blueprint $table) {
+        Schema::create('time_trackers', function (Blueprint $table) {
             $table->increments('id');
-
-            $table->unsignedInteger('campaign_id');
-            $table->foreign('campaign_id')->references('id')->on('campaigns');
-
-            $table->unsignedInteger('parent_campaign_id');
-            $table->foreign('parent_campaign_id')->references('id')->on('campaigns');
 
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
 
-            $table->text('message');
-            $table->text('data')->nullable();
+            $table->dateTime('time_in');
+            $table->dateTime('time_out')->nullable();
+
+            $table->string('reason')->nullable();
+
+            $table->string('type')->nullable();
 
             $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
         });
     }
 
@@ -39,6 +39,6 @@ class CreateCampaignHistoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('campaign_histories');
+        Schema::dropIfExists('time_trackers');
     }
 }
