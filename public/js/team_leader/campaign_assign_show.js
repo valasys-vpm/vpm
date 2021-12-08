e24369721a89cd01f7c9e500effb3a001c51fcfa
valasys-vpm <87777274+valasys-vpm@users.noreply.github.com>
2021-12-09 00:48:08 +0530

    testing

--- public/js/team_leader/campaign_assign_show.js
+++ public/js/team_leader/campaign_assign_show.js
@@ -152,6 +152,58 @@ function viewAssignmentDetails(id) {
     });
 }
 
+function viewAgentLeadDetails(_ca_agent_id) {
+    $.ajax({
+        type: 'get',
+        url: $('meta[name="base-path"]').attr('content') + '/team-leader/campaign/view-agent-lead-details/' + _ca_agent_id,
+        success: function (response){
+            if(response.status === true) {
+                console.log(response.data);
+                let data = null;
+                let html = '';
+
+                if(response.data.length) {
+                    $.each(response.data, function (key, value) {
+                        html += '' +
+                            '<tr>\n' +
+                            '   <td>' + (key + 1) + '</td>\n' +
+                            '   <td>' + value.first_name + '</td>\n' +
+                            '   <td>' + value.last_name + '</td>\n' +
+                            '   <td>' + value.company_name + '</td>\n' +
+                            '   <td>' + value.email_address + '</td>\n' +
+                            '   <td>' + value.phone_number + '</td>\n' +
+                            '   <td>' + value.address_1 + '</td>\n' +
+                            '   <td>' + value.address_2 + '</td>\n' +
+                            '   <td>' + value.city + '</td>\n' +
+                            '   <td>' + value.state + '</td>\n' +
+                            '   <td>' + value.zipcode + '</td>\n' +
+                            '   <td>' + value.country + '</td>\n' +
+                            '   <td>' + value.employee_size + '</td>\n' +
+                            '   <td>' + value.revenue + '</td>\n' +
+                            '   <td>' + value.company_domain + '</td>\n' +
+                            '   <td>' + value.website + '</td>\n' +
+                            '   <td>' + value.company_linkedin_url + '</td>\n' +
+                            '   <td>' + value.linkedin_profile_link + '</td>\n' +
+                            '   <td>' + value.linkedin_profile_sn_link + '</td>\n' +
+                            '   <td>' + moment(value.created_at).format('DD-MMM-YYYY') + '</td>\n' +
+                            '</tr>';
+                        '';
+                    });
+
+                    $("#modal-view-lead-details").find('tbody').html(html);
+
+                    $("#modal-view-lead-details").modal('show');
+
+                } else {
+                    trigger_pnofify('warning', 'Error while processing request', 'Data Not Found');
+                }
+            } else {
+                trigger_pnofify('error', 'Error while processing request', response.message);
+            }
+        }
+    });
+}
+
 function submitCampaign(_id) {
     if(_id && confirm('Are you sure to submit campaign?')) {
         $.ajax({
