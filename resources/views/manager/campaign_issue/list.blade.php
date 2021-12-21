8692324f67a512485fc8a16ed27b4872b52c26b4
valasys-vpm <87777274+valasys-vpm@users.noreply.github.com>
2021-12-22 02:44:08 +0530

    manager campaign issue changed modal name to close issue

--- resources/views/manager/campaign_issue/list.blade.php
+++ resources/views/manager/campaign_issue/list.blade.php
@@ -84,7 +84,7 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Raise Issue</h5>
+                    <h5 class="modal-title">Close Issue</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 </div>
                 <div class="modal-body">
