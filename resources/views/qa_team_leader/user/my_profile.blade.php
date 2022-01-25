59038aa7da3150620f13794b8772a2ee7eeb2eb4
valasys-vpm <87777274+valasys-vpm@users.noreply.github.com>
2022-01-25 21:47:16 +0530

    js changes

--- resources/views/qa_team_leader/user/my_profile.blade.php
+++ resources/views/qa_team_leader/user/my_profile.blade.php
@@ -193,6 +193,6 @@
     @parent
     <!-- jquery-validation Js -->
     <script src="{{ asset('public/template/assets/plugins/jquery-validation/js/jquery.validate.min.js') }}"></script>
-    <script src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/additional-methods.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.0/additional-methods.min.js"></script>
     <script src="{{ asset('public/js/qa_team_leader/my_profile.js?='.time()) }}"></script>
 @append
