fdcda577ae8ca22def835e87a7acee58ba028d7e
Sagar Patil <vpm@valasys.com>
2022-02-04 22:17:54 +0530

    accept 0 value for lead approved and lead availabel

--- app/Repository/Campaign/DeliveryDetailRepository/DeliveryDetailRepository.php
+++ app/Repository/Campaign/DeliveryDetailRepository/DeliveryDetailRepository.php
@@ -22,32 +22,28 @@ class DeliveryDetailRepository implements DeliveryDetailInterface
 
     public function get($filters = array())
     {
-        // TODO: Implement get() method.
+        $query = CampaignDeliveryDetail::query();
+        return $query->get();
     }
 
     public function find($id)
     {
-        // TODO: Implement find() method.
+        $query = CampaignDeliveryDetail::query();
+        return $query->find($id);
     }
 
     public function store($attributes)
-    {
-        // TODO: Implement store() method.
-    }
-
-    public function update($id = 0, $attributes)
     {
         $response = array('status' => FALSE, 'message' => 'Something went wrong, please try again.');
         try {
             DB::beginTransaction();
 
+            $delivery_detail = new CampaignDeliveryDetail();
 
-            $delivery_detail = CampaignDeliveryDetail::findOrNew($id);
-
-            if(!empty($delivery_detail->campaign_id)) {
-                $resultCampaign = Campaign::findOrFail($delivery_detail->campaign_id);
-            } else {
+            if(isset($attributes['campaign_id']) && !empty($attributes['campaign_id'])) {
                 $resultCampaign = Campaign::findOrFail($attributes['campaign_id']);
+            } else {
+                $resultCampaign = Campaign::findOrFail($delivery_detail->campaign_id);
             }
 
             if(isset($attributes['campaign_id']) && !empty($attributes['campaign_id'])) {
@@ -65,6 +61,62 @@ class DeliveryDetailRepository implements DeliveryDetailInterface
             $delivery_detail->lead_rejected = $delivery_detail->lead_sent - $delivery_detail->lead_approved;
 
             if(isset($attributes['lead_available']) && !empty($attributes['lead_available'])) {
+                $delivery_detail->lead_available = $attributes['lead_available'];
+            }
+
+            $delivery_detail->lead_pending = $resultCampaign->allocation - $delivery_detail->lead_approved;
+
+            if(isset($attributes['campaign_progress']) && !empty($attributes['campaign_progress'])) {
+                $delivery_detail->campaign_progress = $attributes['campaign_progress'];
+            }
+
+            if(isset($attributes['updated_by']) && !empty($attributes['updated_by'])) {
+                $delivery_detail->updated_by = $attributes['updated_by'];
+            }
+
+            $delivery_detail->save();
+            if($delivery_detail->id) {
+                DB::commit();
+                $response = array('status' => TRUE, 'message' => 'Campaign delivery details updated successfully');
+            } else {
+                throw new \Exception('Something went wrong, please try again.', 1);
+            }
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            $response = array('status' => FALSE, 'message' => 'Something went wrong, please try again.');
+        }
+        return $response;
+    }
+
+    public function update($id = 0, $attributes)
+    {
+        $response = array('status' => FALSE, 'message' => 'Something went wrong, please try again.');
+        try {
+            DB::beginTransaction();
+
+            $delivery_detail = CampaignDeliveryDetail::findOrNew($id);
+
+            if(!empty($delivery_detail->campaign_id)) {
+                $resultCampaign = Campaign::findOrFail($delivery_detail->campaign_id);
+            } else {
+                $resultCampaign = Campaign::findOrFail($attributes['campaign_id']);
+            }
+
+            if(isset($attributes['campaign_id']) && !empty($attributes['campaign_id'])) {
+                $delivery_detail->campaign_id = $attributes['campaign_id'];
+            }
+
+            if(isset($attributes['lead_sent']) && !empty($attributes['lead_sent'])) {
+                $delivery_detail->lead_sent = $attributes['lead_sent'];
+            }
+
+            if(array_key_exists('lead_approved', $attributes)) {
+                $delivery_detail->lead_approved = $attributes['lead_approved'];
+            }
+
+            $delivery_detail->lead_rejected = $delivery_detail->lead_sent - $delivery_detail->lead_approved;
+
+            if(array_key_exists('lead_available', $attributes)) {
                 $delivery_detail->lead_available = $attributes['lead_available'];
             }
 
