ab577e5ee690c716531b43d3ddb48bdb5b9093fc
Sagar Patil <vpm@valasys.com>
2022-02-09 02:13:10 +0530

    BR: VPM-163

--- resources/views/manager/campaign/create.blade.php
+++ resources/views/manager/campaign/create.blade.php
@@ -20,6 +20,9 @@
                                 <div class="col-md-12">
                                     <div class="page-header-title">
                                         <h5 class="m-b-10">Campaign Management</h5>
+                                        <div class="card-header-right mb-1" style="float: right;">
+                                            <a href="{{ route('manager.campaign.list') }}" class="btn btn-outline-info btn-square btn-sm pt-1 pb-1" style="font-weight: bold;"><i class="feather icon-arrow-left"></i>Back</a>
+                                        </div>
                                     </div>
                                     <ul class="breadcrumb">
                                         <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}"><i class="feather icon-home"></i></a></li>
