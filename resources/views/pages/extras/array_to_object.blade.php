b10bbd94f533233f4136d7e75455f95af9cc2163
Sagar Patil <vpm@valasys.com>
2022-02-17 01:46:20 +0530

    testing

--- resources/views/pages/extras/array_to_object.blade.php
+++ resources/views/pages/extras/array_to_object.blade.php
@@ -11,8 +11,26 @@
 
         @if(isset($result) && !empty($result))
         <div style="margin-top: 20px;width: 80%;word-wrap: break-word;">
-            <textarea cols="100" rows="100">{{ $result.';' }}</textarea>
+            <button onclick="myFunction();" style="cursor: pointer;">Copy text</button>
+            <br>
+            <textarea cols="100" rows="100" id="myInput">{{ $result.';' }}</textarea>
         </div>
         @endif
     </body>
+    <script>
+        function myFunction() {
+            /* Get the text field */
+            var copyText = document.getElementById("myInput");
+
+            /* Select the text field */
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+            /* Copy the text inside the text field */
+            navigator.clipboard.writeText(copyText.value);
+
+            /* Alert the copied text */
+            //alert("Copied the text: " + copyText.value);
+        }
+    </script>
 </html>
