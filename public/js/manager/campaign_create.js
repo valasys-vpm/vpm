2b6eaca9463e8429000e6ab390b433f3a1124fce
Sagar Patil <vpm@valasys.com>
2022-02-18 01:51:28 +0530

    BR: VPM-221

--- public/js/manager/campaign_create.js
+++ public/js/manager/campaign_create.js
@@ -84,8 +84,8 @@ $(function (){
     });
 
     //update sub-allocation total count
-    $("#allocation").on('keyup', function () {
-        $(this).prop('checked', false);
+    $("#allocation").on('keyup, change', function () {
+        $('input[name="pacing"]').prop('checked', false);
         resetPacingDetails();
         let allocation = ($(this).val() > 0) ? $(this).val() : '0';
         $("#text-allocation").html(' / '+allocation);
